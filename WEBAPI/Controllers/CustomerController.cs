3e18894fe1b45e1318bdafb20d278f689a46f74b
Ramya <ramyakrc10@gmail.com>
2018-03-26 19:32:52 -0500

    Add project files.

---
+++ WEBAPI/Controllers/CustomerController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WEBAPI.Models;
+
+namespace WEBAPI.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        private rammiEntities test = new rammiEntities();
+
+        // GET api/<controller>
+        public IEnumerable<CUSTOMER> Get()
+        {
+            return test.CUSTOMERs;
+        }
+
+        // GET api/<controller>/5
+        public CUSTOMER Get(int id)
+           
+        {
+            
+            return test.CUSTOMERs.FirstOrDefault(X => X.CUST_NAME=="GOPI");
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+          
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
